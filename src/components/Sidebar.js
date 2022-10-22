2eb1774b168b364118a36eab2db595b28852e93e
sivanov <sivanov@2create.studio>
2022-10-22 12:46:07 +0300

    Check for current path

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,30 +2,37 @@
  * External dependencies
  */
 import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import { Link } from 'react-router-dom';
+import { Link, Location, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 /**
  * Internal dependencies
  */
 
 const SidebarLayout = () => {
+    const location = useLocation();
+    const [path, setPath] = useState(location.pathname);
+
+    useEffect(() => {
+        setPath(location.pathname);
+    }, [location])
     return (
         <div style={{ display: 'flex', height: '100%' }}>
             <Sidebar>
                 <Menu>
-                    <MenuItem>
+                    <MenuItem active={path === '/'}>
                         <Link to="/">
                             All posts
                         </Link>
                     </MenuItem>
 
-                    <MenuItem>
+                    <MenuItem active={path === '/my-posts'}>
                         <Link to="/my-posts">
                             My posts
                         </Link>
                     </MenuItem>
 
-                    <MenuItem>
+                    <MenuItem active={path === '/create-post'}>
                         <Link to="/create-post">
                             Create post
                         </Link>
